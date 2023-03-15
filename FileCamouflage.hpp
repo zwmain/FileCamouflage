6c26d9506524a2003f1ad118847833e65db970c9
zwmain <42022704+zwmain@users.noreply.github.com>
2023-03-15 21:00:58 +0800

    Finish base logic
    * Modify CMakeLists file to support more situation
    
    * Add logic of convert file to image
    
    * Fix bug that can not read file with binary
    
    * Add logic to reback file from images
    
    * Finsh base logic

--- FileCamouflage.hpp
+++ FileCamouflage.hpp
@@ -1,23 +1,103 @@
 #ifndef _FILE_CAMOUFLAGE_HPP_
 #define _FILE_CAMOUFLAGE_HPP_
 
+#include <cstring>
+#include <filesystem>
+#include <fmt/format.h>
 #include <fstream>
 #include <iostream>
+#include <memory>
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <string>
+#include <vector>
 
 namespace zwn {
 
+namespace fs = std::filesystem;
+
 constexpr size_t K1_WIDTH = 1920;
 constexpr size_t K1_HEIGH = 1080;
-constexpr size_t K1_SIZE = 2073600;
+constexpr size_t K1_SIZE = 6220800;
+constexpr size_t K1_BUFF = 6220792;
+const std::vector<int> PNG_CFG = { cv::IMWRITE_PNG_COMPRESSION, 0, cv::IMWRITE_PNG_STRATEGY, cv::IMWRITE_PNG_STRATEGY_DEFAULT };
 
 void pngMatType(const std::string& fp)
 {
     cv::Mat img = cv::imread(fp);
     int tp = img.type();
     std::cout << "Type: " << tp << std::endl;
+}
+
+void convertFiltToImage(const std::string& inputFile, const std::string& outputDir)
+{
+    fs::path fp(inputFile);
+    if (!fs::exists(fp)) {
+        return;
+    }
+    if (!fs::is_regular_file(fp)) {
+        return;
+    }
+    fs::path dir(outputDir);
+    if (!fs::exists(dir)) {
+        bool isOk = fs::create_directory(dir);
+        if (!isOk) {
+            return;
+        }
+    } else {
+        if (!fs::is_directory(dir)) {
+            return;
+        }
+    }
+
+    uint64_t fileSize = fs::file_size(fp);
+    size_t fileCnt = fileSize / K1_BUFF;
+    if (fileSize % K1_BUFF != 0) {
+        fileCnt += 1;
+    }
+
+    std::ifstream fi(fp, std::ios::in | std::ios::binary);
+    if (!fi.is_open()) {
+        return;
+    }
+    std::string fileName = fp.filename().string();
+    cv::Mat img = cv::Mat::zeros(K1_HEIGH, K1_WIDTH, CV_8UC3);
+    uint64_t remainSize = fileSize;
+    std::unique_ptr<char[]> buf = std::make_unique<char[]>(K1_BUFF);
+    for (size_t i = 0; i < fileCnt; ++i) {
+        uchar* data = img.data;
+        uint64_t wrSize = remainSize > K1_BUFF ? K1_BUFF : remainSize;
+        std::memcpy(data, &wrSize, sizeof(wrSize));
+        fi.read(buf.get(), wrSize);
+        std::memcpy(&data[8], buf.get(), wrSize);
+        remainSize -= wrSize;
+        std::string outName = fmt::format("{}_{}.png", fileName, i);
+        fs::path outPath = dir / outName;
+        cv::imwrite(outPath.string(), img, PNG_CFG);
+    }
+}
+
+void convertImagesToFile(const std::string& inputDir, const std::string& outputFile)
+{
+    fs::path dirPath(inputDir);
+    if (!fs::exists(dirPath) || !fs::is_directory(dirPath)) {
+        return;
+    }
+    std::ofstream fo(outputFile, std::ios::out | std::ios::binary | std::ios::app);
+    if (!fo.is_open()) {
+        return;
+    }
+    fs::directory_iterator itDir(dirPath);
+    for (const fs::directory_entry& p : itDir) {
+        std::cout << p.path() << std::endl;
+        cv::Mat img = cv::imread(p.path().string());
+        if (!(img.rows == K1_HEIGH && img.cols == K1_WIDTH)) {
+            continue;
+        }
+        uint64_t dataSz = 0;
+        std::memcpy(&dataSz, img.data, sizeof(dataSz));
+        fo.write((char*)&img.data[8], dataSz);
+    }
 }
 
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include "FileCamouflage.hpp"
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
     std::cout << "文件伪装" << std::endl;
-    zwn::pngMatType("D:\\Pictures\\Saved Pictures\\Snipaste_2022-12-19_20-08-15.png");
+    zwn::convertFiltToImage("D:/ProgramFiles/v2rayN.7z", "D:/Temp/Images");
+    zwn::convertImagesToFile("D:/Temp/Images", "D:/Temp/v2rayN.7z");
     return 0;
 }
