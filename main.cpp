444477fc6d9158e00f4cc3cc87083f15562dba7f
zwmain <42022704+zwmain@users.noreply.github.com>
2023-03-19 14:28:36 +0800

    Finish file to image (#4)
    * Modify CMakeLists file to support more situation
    
    * Add logic of convert file to image
    
    * Fix bug that can not read file with binary
    
    * Add logic to reback file from images
    
    * Finsh base logic
    
    * Format code
    
    * Add function: makeImageStrategy getNumWidth
    
    * Add function fileToImages
    
    * Add function: readFileToImage
    
    * Use readFileToImage
    
    * Modify recoveryFile
    
    * Fix bug for file pos
    
    * Finish most functions bug the regex crash
    
    * Fix regex error
    
    * Remove cout
    
    * Add multi-threads support

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include "FileCamouflage.hpp"
+#include <chrono>
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
+    zwn::ThreadPool::instance()->start();
     std::cout << "文件伪装" << std::endl;
+    auto curTime = std::chrono::system_clock::now();
     zwn::disguiseFile("D:/ProgramFiles/v2rayN.7z", "D:/Temp/Images");
+    auto midTime = std::chrono::system_clock::now();
+    auto durTime = midTime - curTime;
+    size_t durVal = std::chrono::duration_cast<std::chrono::milliseconds>(durTime).count();
+    std::cout << "disguiseFile: " << durVal << std::endl;
     zwn::recoveryFile("D:/Temp/Images", "D:/Temp/v2rayN.7z");
+    auto endTime = std::chrono::system_clock::now();
+    durTime = endTime - midTime;
+    durVal = std::chrono::duration_cast<std::chrono::milliseconds>(durTime).count();
+    std::cout << "recoveryFile: " << durVal << std::endl;
     return 0;
 }
